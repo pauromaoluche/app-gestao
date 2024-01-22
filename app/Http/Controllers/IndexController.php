3dc5c1ea85953295f2999c7914794e5ad26bbf52
pauluci <pauluci125romao.pedro@hotmail.com>
2024-01-22 18:16:34 -0300

    Implementando controllers principais, sobrenos e contato

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+   public function index(){
+    echo "Olá, seja bem vindo ao curso";
+   }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,16 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Olá, seja bem vindo ao curso';
-});
+Route::get('/', [IndexController::class, 'index']);
 
-Route::get('/sobre-nos', function () {
-    return 'Sobre-nos';
-});
+Route::get('/sobre-nos', [SobreNosController::class, 'index']);
 
-Route::get('/contato', function () {
-    return 'Contato';
-});
-
+Route::get('/contato',[ContatoController::class, 'index']);
 
