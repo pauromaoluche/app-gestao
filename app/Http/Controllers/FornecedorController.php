14ea2aaa3b2cf7e8bb422937634b35ddb3718f03
pauluci <pauluci125romao.pedro@hotmail.com>
2024-01-23 08:42:46 -0300

    Verificador ternario

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -9,7 +9,10 @@ class FornecedorController extends Controller
     public function index(){
         $fornecedores = [
             0=>['nome' =>'Fornecedor 1', 'status' =>'N', 'cnpj' => '' ],
-            1=>['nome' =>'Fornecedor 2', 'status' =>'S']];
+            1=>['nome' =>'Fornecedor 2', 'status' =>'S', 'cnpj' => '215123']];
+
+            $msg = isset($fornecedores[1]['cnpj']) ? 'CNPJ Informado' : 'CNPJ não informado';
+
         return view('app.fornecedor.index', compact('fornecedores'));
        }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -12,6 +12,14 @@
     // }
 @endphp
 
+@if ($fornecedores > 0 && $fornecedores < 10)
+    <h3>Temos alguns fornecedores</h3>
+@elseif ($fornecedores > 10)
+    <h3>Temos muitos fornecedores</h3>
+@else
+    <h3>Não temos nenhum fornecedor</h3>
+@endif
+
 @isset($fornecedores)
     <br>
     Fornecedor: {{ $fornecedores[0]['nome'] }}
