dd7340ce1fafd372ddbea86f1c7684107312282f
pauluci <pauluci125romao.pedro@hotmail.com>
2024-01-23 10:16:04 -0300

    Variavel de loop

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,14 +1,10 @@
 <h3>Fornecedor</h3>
-
 {{ 'Texto de teste' }}
 
 @php
     // if(){
-
     // }elseif(){
-
     // }else{
-
     // }
 @endphp
 
@@ -22,6 +18,9 @@
 
 @isset($fornecedores)
     @forelse ($fornecedores as $indice => $fornecedor)
+        {{-- @dd($loop)     --}}
+
+        Interação atual: {{ $loop->iteration }}
         <br>
         Fornecedor: {{ $fornecedor['nome'] }}
         <br>
@@ -31,6 +30,15 @@
         {{-- Se a variavel testada nao estiver definida ou tiver valor null, ele usa o valor defautl --}}
         <br>
         Telefone: {{ $fornecedor['ddd'] ?? '' }} {{ $fornecedor['telefone'] ?? '' }}
+        <br>
+        @if ($loop->first)
+            Primeira interação do loop
+        @endif
+        @if ($loop->last)
+            Ultima interação do loop
+            <br>
+            Total de registros: {{ $loop->count }}
+        @endif
         <hr>
     @empty
         Não Existem fornecedores cadastrados!!!
