e9a475cff47babad363692b4780d6af2914d05ad
pauluci <pauluci125romao.pedro@hotmail.com>
2024-01-23 09:11:55 -0300

    Utilizando foreach

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -21,23 +21,17 @@
 @endif
 
 @isset($fornecedores)
-    @php
-        $i = 0;
-    @endphp
-    @while (isset($fornecedores[$i]))
+    @foreach ($fornecedores as $indice => $fornecedor)
         <br>
-        Fornecedor: {{ $fornecedores[0]['nome'] }}
+        Fornecedor: {{ $fornecedor['nome'] }}
         <br>
-        Status: {{ $fornecedores[0]['status'] }}
+        Status: {{ $fornecedor['status'] }}
         <br>
-        CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'valor nao esta definida' }}
+        CNPJ: {{ $fornecedor['cnpj'] ?? 'valor nao esta definida' }}
         {{-- Se a variavel testada nao estiver definida ou tiver valor null, ele usa o valor defautl --}}
         <br>
-        Telefone: {{ $fornecedores[1]['ddd'] ?? '' }} {{ $fornecedores[1]['telefone'] ?? '' }}
+        Telefone: {{ $fornecedor['ddd'] ?? '' }} {{ $fornecedor['telefone'] ?? '' }}
         <hr>
-        @php
-            $i++;
-        @endphp
-    @endwhile
+    @endforeach
 @endisset
 <br>
