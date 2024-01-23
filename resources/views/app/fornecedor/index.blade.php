afbf0d54ce9fd11d2d85befa1c77f1c25f154d77
pauluci <pauluci125romao.pedro@hotmail.com>
2024-01-22 23:38:08 -0300

    if else

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -5,5 +5,22 @@
 {{-- Comentario blade --}}
 
 @php
-    //Comentario php
+    // if(){
+
+    // }elseif(){
+
+    // }else{
+
+    // }
 @endphp
+
+{{-- dump and die --}}
+{{-- @dd($fornecedores) --}}
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existe varios fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
