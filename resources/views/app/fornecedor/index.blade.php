da39a32206c733eef7750a796dcbfb17d4641ec3
pauluci <pauluci125romao.pedro@hotmail.com>
2024-01-23 09:09:02 -0300

    Utilizando while

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -21,7 +21,10 @@
 @endif
 
 @isset($fornecedores)
-    @for ($i = 0; isset($fornecedores[$i]); $i++)
+    @php
+        $i = 0;
+    @endphp
+    @while (isset($fornecedores[$i]))
         <br>
         Fornecedor: {{ $fornecedores[0]['nome'] }}
         <br>
@@ -32,6 +35,9 @@
         <br>
         Telefone: {{ $fornecedores[1]['ddd'] ?? '' }} {{ $fornecedores[1]['telefone'] ?? '' }}
         <hr>
-    @endfor
+        @php
+            $i++;
+        @endphp
+    @endwhile
 @endisset
 <br>
