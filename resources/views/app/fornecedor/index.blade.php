7d0d1cf81b4315e111691506cd92186157bf23e6
pauluci <pauluci125romao.pedro@hotmail.com>
2024-01-22 23:53:56 -0300

    Utilizando isset

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,8 +2,6 @@
 
 {{ 'Texto de teste' }}
 
-{{-- Comentario blade --}}
-
 @php
     // if(){
 
@@ -14,21 +12,9 @@
     // }
 @endphp
 
-{{-- dump and die --}}
-{{-- @dd($fornecedores) --}}
-
-{{-- @if (count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existe varios fornecedores cadastrados</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados</h3>
-@endif --}}
-
-@if ($fornecedores[0]['status'] == 'S')
-    <h3>Fornecedor Inativo</h3>
-@endif
-<br>
-@unless ($fornecedores[0]['status'] == 'S') {{-- verifica se o retorno da condição é falso --}}
-    <h3>Fornecedor inativo unless</h3>
-@endunless
+@isset($fornecedores)
+    <br>
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[0]['status'] }}
+@endisset
