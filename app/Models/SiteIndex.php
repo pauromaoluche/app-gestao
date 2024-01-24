662181b1d93d8c90c5e0fd5ec258dd4a8ee89ee8
pauluci <pauluci125romao.pedro@hotmail.com>
2024-01-24 08:37:16 -0300

    Criando primeira migration

--- app/Models/SiteIndex.php
+++ app/Models/SiteIndex.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Index extends Model
+class SiteIndex extends Model
 {
     use HasFactory;
 }
