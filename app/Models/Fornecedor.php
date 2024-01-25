30ecf4c627b536f57e557289ff62dc53149bbf3c
pauluci <pauluci125romao.pedro@hotmail.com>
2024-01-25 20:19:30 -0300

    Utilizando softDelete onde adiciona coluna de delete at onde ao estiver preenchida o registro e entendido como inativo

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
     use HasFactory;
+
+    //adiciona tabela de delete, inativa o registro sem o deletar
+    use SoftDeletes;
 
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
