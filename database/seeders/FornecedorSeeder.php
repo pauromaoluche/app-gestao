16601de0136758e5a070ef4715860d4eef618c7c
pauluci <pauluci125romao.pedro@hotmail.com>
2024-01-25 20:26:32 -0300

    Utilizando Seeders

---
+++ database/seeders/FornecedorSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Fornecedor;
+
+class FornecedorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'fornecedor100.com.br';
+        $fornecedor->uf = 'PR';
+        $fornecedor->email = 'fornecedor100@hotmail.com.br';
+        $fornecedor->save();
+
+    }
+}
