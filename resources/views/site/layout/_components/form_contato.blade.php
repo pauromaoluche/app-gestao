5546fe8d8e62f36d84dae12460d2948f3202bf15
pauluci <pauluci125romao.pedro@hotmail.com>
2024-01-27 18:51:07 -0300

    validando erro individual

--- resources/views/site/layout/_components/form_contato.blade.php
+++ resources/views/site/layout/_components/form_contato.blade.php
@@ -2,10 +2,13 @@
 <form action="{{ route('site.contato') }}" method="POST">
     @csrf
     <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="borda-preta">
+    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
     <br>
     <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="borda-preta">
+    {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}
     <br>
     <input name="email" value="{{ old('email') }}" type="email" placeholder="E-mail" class="borda-preta">
+    {{ $errors->has('email') ? $errors->first('email') : '' }}
     <br>
     <select name="motivo_contato_id" class="borda-preta">
         <option value="">Qual o motivo do contato?</option>
@@ -15,14 +18,17 @@
                 {{ old('motivo_contato_id') == $motivo_contato->id ? 'selected' : '' }}>
                 {{ $motivo_contato->motivo_contato }}</option>
         @endforeach
-
     </select>
+    {{ $errors->has('motivo_contato_id') ? $errors->first('motivo_contato_id') : '' }}
     <br>
     <textarea name="mensagem" class="borda-preta">{{ old('mensagem') != '' ? old('mensagem') : 'Escreve a mensagem aqui' }}
     </textarea>
+    {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}
     <br>
     <button type="submit" class="borda-preta">ENVIAR</button>
 </form>
+
+{{-- Verifica se existe erro no $erros, e depois o percorre mostrando os erros
 @if ($errors->any())
     <div>
         @foreach ($errors->all() as $erro)
@@ -30,4 +36,4 @@
             <br>
         @endforeach
     </div>
-@endif
+@endif --}}
