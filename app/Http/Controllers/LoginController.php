3c9045310f2ece98cd6b0844d2c95641f3c1af0f
pauluci <pauluci125romao.pedro@hotmail.com>
2024-01-29 22:49:02 -0300

    Validando a existencia dos dados no banco

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('site.login.index', ['titulo' => 'Login']);
     }
 
-    public function autenticar(Request $request){
-        
+    public function autenticar(Request $request)
+    {
+
         $regras = [
             'usuario' => 'email',
             'senha' => 'required'
@@ -22,7 +25,26 @@ class LoginController extends Controller
             'senha.required' => 'O campo senha é obrigatorio'
         ];
 
-        $request->validate($regras, $feedback);
+        $email = $request->get('usuario');
+        $senha = $request->get('senha');
 
+        echo "Usuario: $email | Senha: $senha";
+        echo "<br>";
+
+        $user = new User();
+
+        $usuario = $user->where('email', $email)->where('password', $senha)->get()->first();
+
+        if(isset($usuario->name)){
+            echo "Usuario existe";
+        }else{
+            echo "Usuario nao existe";
+        }
+
+        echo "<pre>";
+        print_r($usuario);
+        echo "</pre>";
+
+        $request->validate($regras, $feedback);
     }
 }
