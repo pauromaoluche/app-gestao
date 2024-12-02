126ea6ef8e48a955827a89e40c85d75afbb5a235
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2024-12-02 06:54:14 -0300

    relacionamento 1 pra n produto/fornecedor

---
+++ database/migrations/2024_12_02_094134_alter_produtos_relacionamentos_fornecedores.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('produtos', function (Blueprint $table) {
+
+            $fornecedor_id = DB::table('fornecedores')->insertGetId([
+                'nome' => 'Fornecedor padrão',
+                'site' => 'fornecedorpadrao.com',
+                'uf' => 'PR',
+                'email' => 'fornecedorpadrao@hotmail.com'
+            ]);
+
+            $table->unsignedBigInteger('fornecedor_id')->default($fornecedor_id)->after('id');
+            $table->foreign('fornecedor_id')->references('id')->on('fornecedores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->dropForeign('produtos_fornecedor_id_foreign');
+            $table->dropColumn('fornecedor_id');
+        });
+    }
+};
